2161ca21e3518c545afb02b405c51dfc36a0ce93
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-08-09 21:36:02 +0200

    Fix performance issues after migration to sol2 3.3.0

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -2,193 +2,212 @@
 SETLOCAL
 SET EL=0
 
-ECHO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-SET PROJECT_DIR=%CD%
-ECHO PROJECT_DIR^: %PROJECT_DIR%
 ECHO NUMBER_OF_PROCESSORS^: %NUMBER_OF_PROCESSORS%
 
-
-:: Check CMake version
-SET CMAKE_VERSION=3.16.3
-SET PATH=%PROJECT_DIR%\cmake-%CMAKE_VERSION%-win32-x86\bin;%PATH%
-ECHO cmake^: && cmake --version
-IF %ERRORLEVEL% NEQ 0 ECHO CMAKE not found && GOTO CMAKE_NOT_OK
-
-cmake --version | findstr /C:%CMAKE_VERSION% && GOTO CMAKE_OK
-
-:CMAKE_NOT_OK
-ECHO CMAKE NOT OK - downloading new CMake %CMAKE_VERSION%
-powershell Invoke-WebRequest https://cmake.org/files/v3.16/cmake-%CMAKE_VERSION%-win32-x86.zip -OutFile $env:PROJECT_DIR\cm.zip
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-IF NOT EXIST cmake-%CMAKE_VERSION%-win32-x86 7z -y x cm.zip | %windir%\system32\FIND "ing archive"
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-:CMAKE_OK
-ECHO CMAKE_OK
-cmake --version
-
-ECHO activating VS command prompt ...
 SET PATH=C:\Program Files (x86)\MSBuild\15.0\Bin;%PATH%
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-ECHO platform^: %platform%
-
 ECHO cl.exe version
 cl
 ECHO msbuild version
 msbuild /version
 
-:: HARDCODE "x64" as it is uppercase on AppVeyor and download from S3 is case sensitive
-SET DEPSPKG=osrm-deps-win-x64-14.2-2019.01.7z
-
-:: local development
-ECHO.
-ECHO LOCAL_DEV^: %LOCAL_DEV%
-IF NOT DEFINED LOCAL_DEV SET LOCAL_DEV=0
-IF DEFINED LOCAL_DEV IF %LOCAL_DEV% EQU 1 IF EXIST %DEPSPKG% ECHO skipping deps download && GOTO SKIPDL
-
-IF EXIST %DEPSPKG% DEL %DEPSPKG%
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-ECHO downloading %DEPSPKG%
-powershell Invoke-WebRequest http://project-osrm.wolt.com/windows-build-deps/$env:DEPSPKG -OutFile $env:PROJECT_DIR\$env:DEPSPKG
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-:SKIPDL
-
-IF EXIST osrm-deps ECHO deleting osrm-deps... && RD /S /Q osrm-deps
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-IF EXIST build ECHO deleting build dir... && RD /S /Q build
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-7z -y x %DEPSPKG% | %windir%\system32\FIND "ing archive"
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-::tree osrm-deps
-
-MKDIR build
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+mkdir build
 cd build
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+cmake -DENABLE_CONAN=ON  -G "Visual Studio 16 2019" ..
+msbuild OSRM.sln /p:Configuration=Release /p:Platform=x64 /t:rebuild /p:BuildInParallel=true /m:%NUMBER_OF_PROCESSORS% /toolsversion:Current /p:PlatformToolset=v142 /clp:Verbosity=normal /nologo /flp1:logfile=build_errors.txt;errorsonly /flp2:logfile=build_warnings.txt;warningsonly
+    
 
-SET OSRMDEPSDIR=%PROJECT_DIR%/osrm-deps
-set PREFIX=%OSRMDEPSDIR%/libs
-set BOOST_ROOT=%OSRMDEPSDIR%
-set BOOST_LIBRARYDIR=%BOOST_ROOT%/lib
-set TBB_INSTALL_DIR=%OSRMDEPSDIR%
-REM set TBB_ARCH_PLATFORM=intel64/vc17
+@REM @ECHO OFF
+@REM SETLOCAL
+@REM SET EL=0
 
-ECHO OSRMDEPSDIR       ^: %OSRMDEPSDIR%
-ECHO PREFIX            ^: %PREFIX%
-ECHO BOOST_ROOT        ^: %BOOST_ROOT%
-ECHO BOOST_LIBRARYDIR  ^: %BOOST_LIBRARYDIR%
-ECHO TBB_INSTALL_DIR   ^: %TBB_INSTALL_DIR%
-REM ECHO TBB_ARCH_PLATFORM ^: %TBB_ARCH_PLATFORM%
+@REM ECHO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+@REM SET PROJECT_DIR=%CD%
+@REM ECHO PROJECT_DIR^: %PROJECT_DIR%
+@REM ECHO NUMBER_OF_PROCESSORS^: %NUMBER_OF_PROCESSORS%
 
 
-ECHO calling cmake ....
-cmake .. ^
--G "Visual Studio 16 2019" ^
--DBOOST_ROOT=%BOOST_ROOT% ^
--DBOOST_LIBRARYDIR=%BOOST_LIBRARYDIR% ^
--DBoost_ADDITIONAL_VERSIONS=1.73.0 ^
--DBoost_USE_MULTITHREADED=ON ^
--DBoost_USE_STATIC_LIBS=ON ^
--DEXPAT_INCLUDE_DIR=%OSRMDEPSDIR% ^
--DEXPAT_LIBRARY=%OSRMDEPSDIR%/lib/libexpat.lib ^
--DBZIP2_INCLUDE_DIR=%OSRMDEPSDIR% ^
--DBZIP2_LIBRARIES=%OSRMDEPSDIR%/lib/libbz2.lib ^
--DLUA_INCLUDE_DIR=%OSRMDEPSDIR% ^
--DLUA_LIBRARIES=%OSRMDEPSDIR%/lib/lua5.3.5.lib ^
--DZLIB_INCLUDE_DIR=%OSRMDEPSDIR% ^
--DZLIB_LIBRARY=%OSRMDEPSDIR%/lib/libz.lib ^
--DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
--DCMAKE_INSTALL_PREFIX=%PREFIX%
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+@REM :: Check CMake version
+@REM SET CMAKE_VERSION=3.16.3
+@REM SET PATH=%PROJECT_DIR%\cmake-%CMAKE_VERSION%-win32-x86\bin;%PATH%
+@REM ECHO cmake^: && cmake --version
+@REM IF %ERRORLEVEL% NEQ 0 ECHO CMAKE not found && GOTO CMAKE_NOT_OK
 
-ECHO building ...
-msbuild OSRM.sln ^
-/p:Configuration=%Configuration% ^
-/p:Platform=x64 ^
-/t:rebuild ^
-/p:BuildInParallel=true ^
-/m:%NUMBER_OF_PROCESSORS% ^
-/toolsversion:Current ^
-/p:PlatformToolset=v142 ^
-/clp:Verbosity=normal ^
-/nologo ^
-/flp1:logfile=build_errors.txt;errorsonly ^
-/flp2:logfile=build_warnings.txt;warningsonly
-IF %ERRORLEVEL% EQU 1 GOTO ERROR
+@REM cmake --version | findstr /C:%CMAKE_VERSION% && GOTO CMAKE_OK
 
-CD %PROJECT_DIR%\build
-IF %ERRORLEVEL% EQU 1 GOTO ERROR
+@REM :CMAKE_NOT_OK
+@REM ECHO CMAKE NOT OK - downloading new CMake %CMAKE_VERSION%
+@REM powershell Invoke-WebRequest https://cmake.org/files/v3.16/cmake-%CMAKE_VERSION%-win32-x86.zip -OutFile $env:PROJECT_DIR\cm.zip
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+@REM IF NOT EXIST cmake-%CMAKE_VERSION%-win32-x86 7z -y x cm.zip | %windir%\system32\FIND "ing archive"
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-SET PATH=%PROJECT_DIR%\osrm-deps\lib;%PATH%
+@REM :CMAKE_OK
+@REM ECHO CMAKE_OK
+@REM cmake --version
 
-ECHO running extractor-tests.exe ...
-unit_tests\%Configuration%\extractor-tests.exe
-IF %ERRORLEVEL% EQU 1 GOTO ERROR
+@REM ECHO activating VS command prompt ...
+@REM SET PATH=C:\Program Files (x86)\MSBuild\15.0\Bin;%PATH%
+@REM CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-ECHO running contractor-tests.exe ...
-unit_tests\%Configuration%\contractor-tests.exe
-IF %ERRORLEVEL% EQU 1 GOTO ERROR
+@REM ECHO platform^: %platform%
 
-ECHO running engine-tests.exe ...
-unit_tests\%Configuration%\engine-tests.exe
-IF %ERRORLEVEL% EQU 1 GOTO ERROR
+@REM ECHO cl.exe version
+@REM cl
+@REM ECHO msbuild version
+@REM msbuild /version
 
-ECHO running util-tests.exe ...
-unit_tests\%Configuration%\util-tests.exe
-IF %ERRORLEVEL% EQU 1 GOTO ERROR
+@REM :: HARDCODE "x64" as it is uppercase on AppVeyor and download from S3 is case sensitive
+@REM SET DEPSPKG=osrm-deps-win-x64-14.2-2019.01.7z
 
-ECHO running server-tests.exe ...
-unit_tests\%Configuration%\server-tests.exe
-IF %ERRORLEVEL% EQU 1 GOTO ERROR
+@REM :: local development
+@REM ECHO.
+@REM ECHO LOCAL_DEV^: %LOCAL_DEV%
+@REM IF NOT DEFINED LOCAL_DEV SET LOCAL_DEV=0
+@REM IF DEFINED LOCAL_DEV IF %LOCAL_DEV% EQU 1 IF EXIST %DEPSPKG% ECHO skipping deps download && GOTO SKIPDL
 
-ECHO running partitioner-tests.exe ...
-unit_tests\%Configuration%\partitioner-tests.exe
-IF %ERRORLEVEL% EQU 1 GOTO ERROR
+@REM IF EXIST %DEPSPKG% DEL %DEPSPKG%
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-ECHO running customizer-tests.exe ...
-unit_tests\%Configuration%\customizer-tests.exe
-IF %ERRORLEVEL% EQU 1 GOTO ERROR
+@REM ECHO downloading %DEPSPKG%
+@REM powershell Invoke-WebRequest http://project-osrm.wolt.com/windows-build-deps/$env:DEPSPKG -OutFile $env:PROJECT_DIR\$env:DEPSPKG
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-ECHO running library-tests.exe ...
-SET test_region=monaco
-SET test_region_ch=ch\monaco
-SET test_region_corech=corech\monaco
-SET test_region_mld=mld\monaco
-SET test_osm=%test_region%.osm.pbf
-IF NOT EXIST %test_osm% powershell Invoke-WebRequest http://project-osrm.wolt.com/testing/monaco.osm.pbf -OutFile %test_osm%
-ECHO running %Configuration%\osrm-extract.exe -p ../profiles/car.lua %test_osm%
-%Configuration%\osrm-extract.exe
-%Configuration%\osrm-extract.exe -p ../profiles/car.lua %test_osm%
-MKDIR ch
-XCOPY %test_region%.osrm.* ch\
-XCOPY %test_region%.osrm ch\
-MKDIR corech
-XCOPY %test_region%.osrm.* corech\
-XCOPY %test_region%.osrm corech\
-MKDIR mld
-XCOPY %test_region%.osrm.* mld\
-XCOPY %test_region%.osrm mld\
-%Configuration%\osrm-contract.exe %test_region_ch%.osrm
-%Configuration%\osrm-contract.exe --core 0.8 %test_region_corech%.osrm
-%Configuration%\osrm-partition.exe %test_region_mld%.osrm
-%Configuration%\osrm-customize.exe %test_region_mld%.osrm
-XCOPY /Y ch\*.* ..\test\data\ch\
-XCOPY /Y corech\*.* ..\test\data\corech\
-XCOPY /Y mld\*.* ..\test\data\mld\
-unit_tests\%Configuration%\library-tests.exe
+@REM :SKIPDL
 
-:ERROR
-ECHO ~~~~~~~~~~~~~~~~~~~~~~ ERROR %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ECHO ERRORLEVEL^: %ERRORLEVEL%
-SET EL=%ERRORLEVEL%
+@REM IF EXIST osrm-deps ECHO deleting osrm-deps... && RD /S /Q osrm-deps
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+@REM IF EXIST build ECHO deleting build dir... && RD /S /Q build
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-:DONE
-ECHO ~~~~~~~~~~~~~~~~~~~~~~ DONE %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@REM 7z -y x %DEPSPKG% | %windir%\system32\FIND "ing archive"
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-EXIT /b %EL%
+@REM ::tree osrm-deps
+
+@REM MKDIR build
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+@REM cd build
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+@REM SET OSRMDEPSDIR=%PROJECT_DIR%/osrm-deps
+@REM set PREFIX=%OSRMDEPSDIR%/libs
+@REM set BOOST_ROOT=%OSRMDEPSDIR%
+@REM set BOOST_LIBRARYDIR=%BOOST_ROOT%/lib
+@REM set TBB_INSTALL_DIR=%OSRMDEPSDIR%
+@REM REM set TBB_ARCH_PLATFORM=intel64/vc17
+
+@REM ECHO OSRMDEPSDIR       ^: %OSRMDEPSDIR%
+@REM ECHO PREFIX            ^: %PREFIX%
+@REM ECHO BOOST_ROOT        ^: %BOOST_ROOT%
+@REM ECHO BOOST_LIBRARYDIR  ^: %BOOST_LIBRARYDIR%
+@REM ECHO TBB_INSTALL_DIR   ^: %TBB_INSTALL_DIR%
+@REM REM ECHO TBB_ARCH_PLATFORM ^: %TBB_ARCH_PLATFORM%
+
+
+@REM ECHO calling cmake ....
+@REM cmake .. ^
+@REM -G "Visual Studio 16 2019" ^
+@REM -DBOOST_ROOT=%BOOST_ROOT% ^
+@REM -DBOOST_LIBRARYDIR=%BOOST_LIBRARYDIR% ^
+@REM -DBoost_ADDITIONAL_VERSIONS=1.73.0 ^
+@REM -DBoost_USE_MULTITHREADED=ON ^
+@REM -DBoost_USE_STATIC_LIBS=ON ^
+@REM -DEXPAT_INCLUDE_DIR=%OSRMDEPSDIR% ^
+@REM -DEXPAT_LIBRARY=%OSRMDEPSDIR%/lib/libexpat.lib ^
+@REM -DBZIP2_INCLUDE_DIR=%OSRMDEPSDIR% ^
+@REM -DBZIP2_LIBRARIES=%OSRMDEPSDIR%/lib/libbz2.lib ^
+@REM -DLUA_INCLUDE_DIR=%OSRMDEPSDIR% ^
+@REM -DLUA_LIBRARIES=%OSRMDEPSDIR%/lib/lua5.3.5.lib ^
+@REM -DZLIB_INCLUDE_DIR=%OSRMDEPSDIR% ^
+@REM -DZLIB_LIBRARY=%OSRMDEPSDIR%/lib/libz.lib ^
+@REM -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
+@REM -DCMAKE_INSTALL_PREFIX=%PREFIX%
+@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+@REM ECHO building ...
+@REM msbuild OSRM.sln ^
+@REM /p:Configuration=%Configuration% ^
+@REM /p:Platform=x64 ^
+@REM /t:rebuild ^
+@REM /p:BuildInParallel=true ^
+@REM /m:%NUMBER_OF_PROCESSORS% ^
+@REM /toolsversion:Current ^
+@REM /p:PlatformToolset=v142 ^
+@REM /clp:Verbosity=normal ^
+@REM /nologo ^
+@REM /flp1:logfile=build_errors.txt;errorsonly ^
+@REM /flp2:logfile=build_warnings.txt;warningsonly
+@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
+
+@REM CD %PROJECT_DIR%\build
+@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
+
+@REM SET PATH=%PROJECT_DIR%\osrm-deps\lib;%PATH%
+
+@REM ECHO running extractor-tests.exe ...
+@REM unit_tests\%Configuration%\extractor-tests.exe
+@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
+
+@REM ECHO running contractor-tests.exe ...
+@REM unit_tests\%Configuration%\contractor-tests.exe
+@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
+
+@REM ECHO running engine-tests.exe ...
+@REM unit_tests\%Configuration%\engine-tests.exe
+@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
+
+@REM ECHO running util-tests.exe ...
+@REM unit_tests\%Configuration%\util-tests.exe
+@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
+
+@REM ECHO running server-tests.exe ...
+@REM unit_tests\%Configuration%\server-tests.exe
+@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
+
+@REM ECHO running partitioner-tests.exe ...
+@REM unit_tests\%Configuration%\partitioner-tests.exe
+@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
+
+@REM ECHO running customizer-tests.exe ...
+@REM unit_tests\%Configuration%\customizer-tests.exe
+@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
+
+@REM ECHO running library-tests.exe ...
+@REM SET test_region=monaco
+@REM SET test_region_ch=ch\monaco
+@REM SET test_region_corech=corech\monaco
+@REM SET test_region_mld=mld\monaco
+@REM SET test_osm=%test_region%.osm.pbf
+@REM IF NOT EXIST %test_osm% powershell Invoke-WebRequest http://project-osrm.wolt.com/testing/monaco.osm.pbf -OutFile %test_osm%
+@REM ECHO running %Configuration%\osrm-extract.exe -p ../profiles/car.lua %test_osm%
+@REM %Configuration%\osrm-extract.exe
+@REM %Configuration%\osrm-extract.exe -p ../profiles/car.lua %test_osm%
+@REM MKDIR ch
+@REM XCOPY %test_region%.osrm.* ch\
+@REM XCOPY %test_region%.osrm ch\
+@REM MKDIR corech
+@REM XCOPY %test_region%.osrm.* corech\
+@REM XCOPY %test_region%.osrm corech\
+@REM MKDIR mld
+@REM XCOPY %test_region%.osrm.* mld\
+@REM XCOPY %test_region%.osrm mld\
+@REM %Configuration%\osrm-contract.exe %test_region_ch%.osrm
+@REM %Configuration%\osrm-contract.exe --core 0.8 %test_region_corech%.osrm
+@REM %Configuration%\osrm-partition.exe %test_region_mld%.osrm
+@REM %Configuration%\osrm-customize.exe %test_region_mld%.osrm
+@REM XCOPY /Y ch\*.* ..\test\data\ch\
+@REM XCOPY /Y corech\*.* ..\test\data\corech\
+@REM XCOPY /Y mld\*.* ..\test\data\mld\
+@REM unit_tests\%Configuration%\library-tests.exe
+
+@REM :ERROR
+@REM ECHO ~~~~~~~~~~~~~~~~~~~~~~ ERROR %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@REM ECHO ERRORLEVEL^: %ERRORLEVEL%
+@REM SET EL=%ERRORLEVEL%
+
+@REM :DONE
+@REM ECHO ~~~~~~~~~~~~~~~~~~~~~~ DONE %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+@REM EXIT /b %EL%
