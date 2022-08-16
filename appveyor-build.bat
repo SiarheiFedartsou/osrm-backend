84db10f735801c876ee7f26df98657714ea99093
Siarhei Fedartsou <siarhei.fedartsou@bolt.eu>
2022-08-16 16:54:56 +0200

    WIP

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -69,10 +69,11 @@ unit_tests\%CONFIGURATION%\customizer-tests.exe
 IF %ERRORLEVEL% EQU 1 GOTO ERROR
 
 SET test_region=monaco
-SET test_region_ch=%PROJECT_DIR%\test\data\ch\monaco
-SET test_region_corech=%PROJECT_DIR%\test\data\corech\monaco
-SET test_region_mld=%PROJECT_DIR%\test\data\mld\monaco
-SET test_osm=%PROJECT_DIR%\test\data\%test_region%.osm.pbf
+SET test_region_ch=ch\monaco
+SET test_region_corech=corech\monaco
+SET test_region_mld=mld\monaco
+SET test_osm=%test_region%.osm.pbf
+COPY %PROJECT_DIR%\test\data\%test_region%.osm.pbf %test_osm% 
 %CONFIGURATION%\osrm-extract.exe -p %PROJECT_DIR%\profiles\car.lua %test_osm%
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
@@ -94,70 +95,6 @@ XCOPY /Y corech\*.* ..\test\data\corech\
 XCOPY /Y mld\*.* ..\test\data\mld\
 unit_tests\%CONFIGURATION%\library-tests.exe
 IF %ERRORLEVEL% EQU 1 GOTO ERROR
-
-@REM ECHO running extractor-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\extractor-tests.exe
-@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-@REM ECHO running contractor-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\contractor-tests.exe
-@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
-
-@REM ECHO running engine-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\engine-tests.exe
-@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
-
-@REM ECHO running util-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\util-tests.exe
-@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
-
-@REM ECHO running server-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\server-tests.exe
-@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
-
-@REM ECHO running partitioner-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\partitioner-tests.exe
-@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
-
-@REM ECHO running customizer-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\customizer-tests.exe
-@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
-@REM echo Exit Code is %errorlevel%
-@REM ECHO running library-tests.exe ...
-@REM SET test_region=monaco
-@REM SET test_region_ch=%PROJECT_DIR%\test\data\ch\monaco
-@REM SET test_region_corech=%PROJECT_DIR%\test\data\corech\monaco
-@REM SET test_region_mld=%PROJECT_DIR%\test\data\mld\monaco
-@REM SET test_osm=%PROJECT_DIR%\test\data\%test_region%.osm.pbf
-@REM ECHO running %CONFIGURATION%\osrm-extract.exe -p ../profiles/car.lua %test_osm%
-@REM cmd /c "exit /b 0"
-@REM %CONFIGURATION%\osrm-extract.exe -p ../profiles/car.lua %test_osm%
-@REM echo Exit Code is %errorlevel%
-@REM @REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-@REM dir Release
-@REM Release\osrm-extract.exe --version
-@REM @rem dir /s /b
-
-@REM MKDIR ch
-@REM XCOPY %test_region%.osrm.* ch\
-@REM XCOPY %test_region%.osrm ch\
-@REM MKDIR corech
-@REM XCOPY %test_region%.osrm.* corech\
-@REM XCOPY %test_region%.osrm corech\
-@REM MKDIR mld
-@REM XCOPY %test_region%.osrm.* mld\
-@REM XCOPY %test_region%.osrm mld\
-@REM %CONFIGURATION%\osrm-contract.exe %test_region_ch%.osrm
-@REM %CONFIGURATION%\osrm-contract.exe --core 0.8 %test_region_corech%.osrm
-@REM %CONFIGURATION%\osrm-partition.exe %test_region_mld%.osrm
-@REM %CONFIGURATION%\osrm-customize.exe %test_region_mld%.osrm
-@REM XCOPY /Y ch\*.* ..\test\data\ch\
-@REM XCOPY /Y corech\*.* ..\test\data\corech\
-@REM XCOPY /Y mld\*.* ..\test\data\mld\
-@REM unit_tests\%CONFIGURATION%\library-tests.exe
-@REM IF %ERRORLEVEL% EQU 1 GOTO ERROR
-
-
 
 :ERROR
 ECHO ~~~~~~~~~~~~~~~~~~~~~~ ERROR %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
