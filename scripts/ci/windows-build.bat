f7b5dd02e7cbd50da59048469ef1356a19da6f99
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-08-23 16:20:21 +0200

    Refactor script

--- scripts/ci/windows-build.bat
+++ scripts/ci/windows-build.bat
@@ -48,45 +48,45 @@ ECHO running util-tests.exe ...
 unit_tests\%CONFIGURATION%\util-tests.exe
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-@REM ECHO running server-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\server-tests.exe
-@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+ECHO running server-tests.exe ...
+unit_tests\%CONFIGURATION%\server-tests.exe
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-@REM ECHO running partitioner-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\partitioner-tests.exe
-@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+ECHO running partitioner-tests.exe ...
+unit_tests\%CONFIGURATION%\partitioner-tests.exe
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-@REM ECHO running customizer-tests.exe ...
-@REM unit_tests\%CONFIGURATION%\customizer-tests.exe
-@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+ECHO running customizer-tests.exe ...
+unit_tests\%CONFIGURATION%\customizer-tests.exe
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-@REM SET test_region=monaco
-@REM SET test_region_ch=ch\monaco
-@REM SET test_region_corech=corech\monaco
-@REM SET test_region_mld=mld\monaco
-@REM SET test_osm=%test_region%.osm.pbf
-@REM COPY %PROJECT_DIR%\test\data\%test_region%.osm.pbf %test_osm% 
-@REM %CONFIGURATION%\osrm-extract.exe -p %PROJECT_DIR%\profiles\car.lua %test_osm%
-@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+SET test_region=monaco
+SET test_region_ch=ch\monaco
+SET test_region_corech=corech\monaco
+SET test_region_mld=mld\monaco
+SET test_osm=%test_region%.osm.pbf
+COPY %PROJECT_DIR%\test\data\%test_region%.osm.pbf %test_osm% 
+%CONFIGURATION%\osrm-extract.exe -p %PROJECT_DIR%\profiles\car.lua %test_osm%
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-@REM MKDIR ch
-@REM XCOPY %test_region%.osrm.* ch\
-@REM XCOPY %test_region%.osrm ch\
-@REM MKDIR corech
-@REM XCOPY %test_region%.osrm.* corech\
-@REM XCOPY %test_region%.osrm corech\
-@REM MKDIR mld
-@REM XCOPY %test_region%.osrm.* mld\
-@REM XCOPY %test_region%.osrm mld\
-@REM %CONFIGURATION%\osrm-contract.exe %test_region_ch%.osrm
-@REM %CONFIGURATION%\osrm-contract.exe --core 0.8 %test_region_corech%.osrm
-@REM %CONFIGURATION%\osrm-partition.exe %test_region_mld%.osrm
-@REM %CONFIGURATION%\osrm-customize.exe %test_region_mld%.osrm
-@REM XCOPY /Y ch\*.* ..\test\data\ch\
-@REM XCOPY /Y corech\*.* ..\test\data\corech\
-@REM XCOPY /Y mld\*.* ..\test\data\mld\
-@REM unit_tests\%CONFIGURATION%\library-tests.exe
-@REM IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+MKDIR ch
+XCOPY %test_region%.osrm.* ch\
+XCOPY %test_region%.osrm ch\
+MKDIR corech
+XCOPY %test_region%.osrm.* corech\
+XCOPY %test_region%.osrm corech\
+MKDIR mld
+XCOPY %test_region%.osrm.* mld\
+XCOPY %test_region%.osrm mld\
+%CONFIGURATION%\osrm-contract.exe %test_region_ch%.osrm
+%CONFIGURATION%\osrm-contract.exe --core 0.8 %test_region_corech%.osrm
+%CONFIGURATION%\osrm-partition.exe %test_region_mld%.osrm
+%CONFIGURATION%\osrm-customize.exe %test_region_mld%.osrm
+XCOPY /Y ch\*.* ..\test\data\ch\
+XCOPY /Y corech\*.* ..\test\data\corech\
+XCOPY /Y mld\*.* ..\test\data\mld\
+unit_tests\%CONFIGURATION%\library-tests.exe
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 :ERROR
 ECHO ~~~~~~~~~~~~~~~~~~~~~~ ERROR %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
