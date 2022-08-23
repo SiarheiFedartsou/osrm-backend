278c653f3a00bbd8a087f5ee65de8410d6bbfb4f
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-08-23 10:44:55 +0200

    Fix test

--- unit_tests/util/viewport.cpp
+++ unit_tests/util/viewport.cpp
@@ -15,7 +15,9 @@ using namespace osrm::util;
 BOOST_AUTO_TEST_CASE(zoom_level_test)
 {
     BOOST_CHECK_EQUAL(
-        12,
+        viewport::getFittedZoom(
+            Coordinate(FloatLongitude{5.668343999999995}, FloatLatitude{45.111511000000014}),
+            Coordinate(FloatLongitude{5.852471999999996}, FloatLatitude{45.26800200000002})),
         12);
 }
 
